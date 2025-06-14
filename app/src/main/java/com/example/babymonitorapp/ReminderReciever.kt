801a7ee4c6a0218f84dcc91dcc9a04299e747349
yogeshveus <yogeshvamci707@gmail.com>
2025-06-14 13:51:38 +0530

    Added database calender, need to implement time and notifs

---
+++ app/src/main/java/com/example/babymonitorapp/ReminderReciever.kt
@@ -0,0 +1,29 @@
+package com.example.babymonitorapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import android.util.Log
+import android.widget.Toast
+
+class ReminderReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val text = intent.getStringExtra("text") ?: "Reminder"
+
+        val builder = NotificationCompat.Builder(context, "reminder_channel")
+            .setSmallIcon(R.mipmap.ic_launcher) // Make sure this exists
+            .setContentTitle("Baby Care Reminder")
+            .setContentText(text)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(System.currentTimeMillis().toInt(), builder.build())
+
+        Log.d("ReminderReceiver", "Broadcast received with text: $text")
+        Toast.makeText(context, "Reminder received: $text", Toast.LENGTH_SHORT).show()
+
+    }
+}
